a4ea447ada508842c78cfe6ce54b14615284ae41
hritiktambe <hrithiktambe@gmail.com>
2019-03-27 06:02:33 +0530

    first commit

---
+++ app/src/main/java/com/hritiktambe/retrofit2/models/News.java
@@ -0,0 +1,45 @@
+package com.hritiktambe.retrofit2.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class News {
+
+    @SerializedName("status")
+    @Expose
+    private String status;
+
+    @SerializedName("totalResults")
+    @Expose
+    private int totalresult;
+
+    @SerializedName("articles")
+    @Expose
+    private List<Article> article;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getTotalresult() {
+        return totalresult;
+    }
+
+    public void setTotalresult(int totalresult) {
+        this.totalresult = totalresult;
+    }
+
+    public List<Article> getArticle() {
+        return article;
+    }
+
+    public void setArticle(List<Article> article) {
+        this.article = article;
+    }
+}
